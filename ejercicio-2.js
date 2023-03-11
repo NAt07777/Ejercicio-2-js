187ecd80eaa1827f05f6c577d70c48606212c9ef
Natalia_Infantino <116117191+NAt07777@users.noreply.github.com>
2023-03-10 22:06:12 -0300

    Add files via upload

---
+++ ejercicio-2.js
@@ -0,0 +1,14 @@
+
+const fechaNac = new Date("february 8 1983");
+console.log(fechaNac);
+
+const libroFavorito = {
+    titulo: "Un mundo feliz",
+    autor: "Aldous Huxley",
+    fecha: 1932,
+    comprar: "https://www.cuspide.com/",
+};
+
+const lista = ["Natalia", 40, true, fechaNac, libroFavorito];
+
+console.log(lista);
